0410500cd3ed82e9db02b5b596703d8ef7117d03
Tea <tea.desouza@gmail.com>
2019-09-10 14:10:04 +0800

    Tabulate global history

--- Editor/Format/LogHistoryFormatter.cs
+++ Editor/Format/LogHistoryFormatter.cs
@@ -4,22 +4,16 @@ using UnityEngine;
 namespace Active.Log{
 public class LogHistoryFormatter : Formatter{
 
+    const int Padding = 12;
+
     public void Append(LogMessage x){
         msg = x;
-        //
-        // First, moving to next line as appropriate.
-        // Leave an empty line if new frame or GO, but not 3 if both
-        // Or, if it's only a new object/component, put a line break
-        if(isNewFrame || isNewGO) Append("\n\n");
-        else if(isNewObject)      Append("\n");
-        //
-        if(isNewFrame)  Append($"{x.frame} -----------------------------\n\n");
-        if(isNewGO)
-            if(x.owner != null) Append($"■ {x.ownerName}\n");
-        if(isNewObject) Append($"[{x.sourceType}]\n");
-                        Append($"{x.message} " );
+        if(isNewFrame) Append( (x.frame + " ").PadRight(40, '―') + '\n' );
+        Append($"{Pad(x.ownerName)} {Pad(x.sourceType)} {x.message}\n");
         Trim();
         prev = x;
     }
+
+    static string Pad(string s) => s.PadRight(Padding);
 
 }}
